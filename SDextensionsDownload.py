74080418f33d082099eb3ebf48a58b0b24d7d4e9
Lyman Zhao <zhaoyunlai@gmail.com>
2023-04-08 11:38:32 +0800

    clean code

--- SDextensionsDownload.py
+++ SDextensionsDownload.py
@@ -10,15 +10,7 @@ extbasePath = Path.joinpath(Path.cwd(),"extensions")
 extgitName=[
             "https://github.com/kohya-ss/sd-webui-additional-networks",#
             "https://github.com/AlUlkesh/stable-diffusion-webui-images-browser",
-            #"https://github.com/hako-mikan/sd-webui-supermerger",
-            "https://github.com/Mikubill/sd-webui-controlnet",
-            #"https://github.com/fkunn1326/openpose-editor.git",
-            #"https://github.com/hnmr293/posex",
-            #"https://github.com/d8ahazard/sd_dreambooth_extension",
-            "https://github.com/jexom/sd-webui-depth-lib",
             "https://github.com/KohakuBlueleaf/a1111-sd-webui-locon",
-            "https://github.com/nonnonstop/sd-webui-3d-open-pose-editor",
-
     ]
 
 
@@ -45,38 +37,14 @@ if vaeModelsPath.exists():
 else:  
     Path.mkdir(vaeModelsPath)
 
-
-
-
-
-#################################################################
-# 下载controlnet Models
-import os
-from pathlib import *
-controlnetModelsPath = Path.joinpath(Path.cwd(),"extensions/sd-webui-controlnet/models") #/content/stable-diffusion-webui/extensions/sd-webui-controlnet/models
-controlnetModels=[
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_hed-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_mlsd-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_normal-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_openpose-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_scribble-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_seg-fp16.safetensors",
-
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_canny-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_color-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_depth-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_keypose-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_openpose-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_seg-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_sketch-fp16.safetensors",
-"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_style-fp16.safetensors",
+vaeModelshugeName=[
+    "https://huggingface.co/stabilityai/sd-vae-ft-ema/resolve/main/diffusion_pytorch_model.safetensors",
 ]
 
-for controlnetModels in controlnetModels:
-    cmd = "cd %s && wget -N -c %s" %(controlnetModelsPath,controlnetModels)
-    subprocess.call(cmd, shell=True)
+if len(vaeModelshugeName) != 0:
+    for vaeModelshugeName in vaeModelshugeName:
+        cmd = "cd %s && wget -N -c  %s" %(vaeModelsPath,vaeModelshugeName)
+        subprocess.call(cmd, shell=True)
 
 
 #####################################################
