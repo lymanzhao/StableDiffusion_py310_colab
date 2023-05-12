6045af7f6a1a11a5f575d0858a77bdfbf6c3de3e
Lyman Zhao <zhaoyunlai@gmail.com>
2023-05-13 02:26:25 +0800

    解决git hash

--- SD_fix_git_commit_hash.py
+++ SD_fix_git_commit_hash.py
@@ -2,26 +2,32 @@ import os
 from pathlib import *
 import subprocess
 currentPath = Path.cwd()
-extbasePath = Path.joinpath(Path.cwd(),"extensions")
+
 #print(extbasePath)
 
-# os.environ["http_proxy"] = "http://127.0.0.1:7890"
-# os.environ["https_proxy"] = "http://127.0.0.1:7890"
+os.environ["http_proxy"] = "http://127.0.0.1:7890"
+os.environ["https_proxy"] = "http://127.0.0.1:7890"
 
 # [path for path in extbasePath.iterdir() if extbasePath.is_dir()]
 # print([path for path in extbasePath.iterdir() if extbasePath.is_dir()])
-
+extbasePath = Path.joinpath(Path.cwd(),"extensions")
 extPath = [path for path in extbasePath.iterdir() if extbasePath.is_dir()]
+
 for extPath in extPath:
+    # print(extPath)
+    extPath = extPath.as_posix()
     print(extPath)
-    cmd = "cd %s && git config --global --add safe.directory %s" %(currentPath,extPath)
+    cmd = "cd %s && git config --global --add safe.directory %s && git pull" %(extPath,extPath)
+    print(cmd)
     subprocess.call(cmd, shell=True)
+
     #print(extPath)
 
 repobasePath = Path.joinpath(Path.cwd(),"repositories")
 repoPath = [path for path in repobasePath.iterdir() if repobasePath.is_dir()]
 for repoPath in repoPath:
-    print(repoPath)
-    cmd = "cd %s && git config --global --add safe.directory %s" %(currentPath,repoPath)
+    # print(repoPath)
+    repoPath=repoPath.as_posix()
+    cmd = "cd %s && git config --global --add safe.directory %s && git pull" %(repoPath,repoPath)
     print(cmd)
     subprocess.call(cmd, shell=True)
